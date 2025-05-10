e96e07331424a9fb4ea5075f14d67c35cf8b7d5e
OkolieOKS234 <okoliedavid42@gmail.com>
2025-05-10 17:59:18 +0100

    added some user friendly styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import { DestinationPickerComponent } from './destination-picker/destination-picker.component';
+import { OutputPageComponent } from './output-page/output-page.component';
+import { TicketListComponent } from "./ticket-list/ticket-list.component";
 
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DestinationPickerComponent},
+    {path: 'page2', component: OutputPageComponent},
+    {path: 'page3', component: TicketListComponent},
+];
